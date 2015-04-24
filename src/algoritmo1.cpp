9880f1f24b889a63a8dbc278c441f6b28cbd56c6
claranobre <aclaranobre@gmail.com>
2015-04-23 21:53:49 -0300

    algoritmo 1, sem funcionar os parenteses

--- src/algoritmo1.cpp
+++ src/algoritmo1.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "../headers/stack.hpp"
 
-char Infx2Posfx (){
+/*char Infx2Posfx (){
 	while(Sequence != top()){
 		if(symb == op1 || symb == op2){
 			top() = symb;
@@ -17,4 +17,31 @@ char Infx2Posfx (){
 			top(symb);
 		}
 	}
+}*/
+
+transformaParaPos(queue &entrada){
+	queue saida;
+	stack operators;
+	string symb, topSymb;
+
+while (!entrada.isEmpty()){
+		symb = entrada.dequeue();
+		if (!isOperator(symb))
+			saida.enqueue(symb);
+		else{
+			while (!operators.isEmpty() && prcd(operators.top(), symb){
+				//if (prcd(operators.top(), symb)){
+					topSymb = operators.pop();
+					saida.enqueue(topSymb);
+				//}
+			}
+			operators.push(symb);
+		}
+
+	}
+	while(!operators.isEmpty()){
+		symb = operators.pop();
+		saida.enqueue(symb);
+	}
+	return saida;
 }
