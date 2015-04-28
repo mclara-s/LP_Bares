8b3cf2796dacd296c96e8e1e0cf87f27390df238
Maria Clara <mclara.sfp@gmail.com>
2015-04-28 12:29:05 -0300

    Tratamento de erros - incompleto

--- src/main.cpp
+++ src/main.cpp
@@ -30,15 +30,56 @@ bool readFile(Queue<string> &expressions){
 
 bool isOperator(string ch){
 	if (ch == "*" || ch == "/" || ch == "+" || ch == "-" || ch == "(" || ch == ")" ||
-		ch == "SIMBOLO PORCENTAGEM" || ch == "$" || ch == "^")
+		ch == "\%" || ch == "$" || ch == "^")
 		return true;
 	else
 		return false;
 }
 
-void tokenizacao (string expr, Queue<string> &tokenQueue){
+bool isNumber(string str){
+	char ch;
+	while (!str.empty()){
+		ch = str.front();
+		if (!isdigit(ch))
+			return false;
+		str.erase(str.begin());
+	}
+	return true;
+}
+
+void errors(int i){
+	switch (i){
+		case 1:
+			cout << "Erro! Constate numerica invalida.\n";
+			break;
+		case 2:
+			cout << "Erro! Falta operando.\n";
+			break;
+		case 3:
+			cout << "Erro! Operando invalido.\n";
+			break;
+		case 4:
+			cout << "Erro! Operador invalido.\n";
+			break;
+		case 5:
+			cout << "Erro! Falta operador.\n";
+			break;
+		case 6:
+			cout << "Erro! Fechamento de escopo invalido.\n";
+			break;
+		case 7:
+			cout << "Erro! Escopo aberto.\n";
+			break;
+		case 8:
+			cout << "Erro! Divisao por zero!\n";
+			break; 
+	}
+}
+
+int tokenizacao (string expr, Queue<string> &tokenQueue){
 	string token, lastToken = "";
 	while (!expr.empty()){
+		token.clear();
 		while (expr.front() != ' ' && expr.front() != '\t' && !expr.empty()){
 			if (isdigit(expr.front())){
 				token += expr.front();
@@ -60,10 +101,19 @@ void tokenizacao (string expr, Queue<string> &tokenQueue){
 		if (!expr.empty() && (expr.front() == ' ' || expr.front() == '\t')){
 			expr.erase(expr.begin());
 		}
+		cout <<"token: "<< token << "lasttoken: " << lastToken << endl;
+		if (isNumber(token) && isNumber(lastToken)){
+			//errors(5);
+			return -1;
+		}
 		lastToken.clear();
 		lastToken = token;
-		token.clear();
 	}
+	if (isOperator(token) && token != ")"){
+		errors(2);
+		return -1;
+	}
+	return 1;
 }
 
 int prioridade(string op){
@@ -80,14 +130,30 @@ int prioridade(string op){
 }
 
 
-void transformaParaPos(Queue<string> &entrada, Queue<string> &saida){
+int transformaParaPos(Queue<string> &entrada, Queue<string> &saida){
 	Stack<string> operators;
 	string symb, topSymb;
+	int op;
 
 	while (!entrada.isEmpty()){
 		symb = entrada.dequeue();
-			if (!isOperator(symb))
-				saida.enqueue(symb);
+			if (!isOperator(symb)){
+				if (isNumber(symb)){
+					op = stoi(symb);
+					if(op > 32767 || op < -32767){
+						errors(1);
+						return -1;
+					}
+					saida.enqueue(symb);
+				}
+				else{
+					if ((symb.front() >= 65 && symb.front() <= 90) || (symb.front() >= 97 && symb.front() <= 122))
+						errors(3);
+					else
+						errors(4);
+					return -1;
+				}
+			}
 			else if (symb == "(")
 				operators.push(symb);
 			else if (symb == ")"){
@@ -115,6 +181,7 @@ void transformaParaPos(Queue<string> &entrada, Queue<string> &saida){
 		symb = operators.pop();
 		saida.enqueue(symb);
 	}
+	return 1;
 }
 
 int main(){
@@ -125,10 +192,12 @@ int main(){
 			infix.clear();
 			posfix.clear();
 			tokenizacao(expressions.dequeue(), infix); //OBS: CORRIGIR TOKENIZAÇÃO
-			infix.print();
+			//infix.print();
 			transformaParaPos(infix, posfix);
-			posfix.print();
-			
+			//posfix.print();
+			//if (transformaParaPos(infix, posfix) > 0){
+				//calcula 
+//			}			
 		}
 	}
 	else{
